3f15d277c8e40ee2bfbfef1afbdb2381ca784294
vedantRaikar <vedantraikar117@gmail.com>
2024-07-30 19:19:03 +0530

    perform ocr added

--- backend/ocr.py
+++ backend/ocr.py
@@ -2,33 +2,30 @@
 import cv2
 import pytesseract
 
-#specify the paths
-pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = cv2.imread(r"C:\Users\vedant raikar\Desktop\ocr health project\tesseract-ocr-project\test files\testimg.jpg")
+def perform_ocr(img_path):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    img = cv2.imread(img_path)
+    img = cv2.resize(img , (500 , 500))
+    text = pytesseract.image_to_string(img)
+    data = pytesseract.image_to_data(img , output_type=pytesseract.Output.DICT)
+    n_boxes = len(data['level'])
+    for i in range(n_boxes):
+        (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        confidence = int(data['conf'][i])
+        if confidence > 0: # Only consider confident detections
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, str(confidence), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-#resize the image
-img = cv2.resize(img, (600, 600))
+    print(text)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()  
+    
+
+        
 
 
-text = pytesseract.image_to_string(img)
+image_path = r"C:\Users\vedant raikar\Desktop\ocr health project\tesseract-ocr-project\test files\testimg.jpg"
+perform_ocr(image_path)
 
 
-
-
-# Extract text and bounding boxes with confidence levels
-data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-print(data)
-
-n_boxes = len(data['level'])
-for i in range(n_boxes):
-    (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-    confidence = int(data['conf'][i])
-    if confidence > 0:  # Only consider confident detections
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, str(confidence), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-cv2.imshow("Image", img)
-
-print(text)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
