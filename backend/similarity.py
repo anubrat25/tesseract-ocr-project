49d4407c05fee461f0f3074536e2a006d3201714
vedantRaikar <vedantraikar117@gmail.com>
2024-12-03 21:41:47 +0530

    Changes

--- backend/similarity.py
+++ backend/similarity.py
@@ -1,7 +1,12 @@
 import requests
+from dotenv import load_dotenv
+import os 
 
+load_dotenv()
+
+hf_key = os.getenv('hf_key')
 API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": hf_key}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
